52711534f2f6e55796e6a9a8f70334fe5e2c9ac5
M.S.Order66 <gj66shadow@gmail.com>
2022-10-24 12:46:50 -0600

    Fixes and Changes kind of

--- GameChooserDemo/Model/Data.swift
+++ GameChooserDemo/Model/Data.swift
@@ -12,9 +12,63 @@ struct Data: Codable, Hashable, Equatable, Comparable {
         lhs.id < rhs.id
     }
     
-    var id: Int
-    var slug: String
-    var name: String
-    var released: String
-    var background_image: String
+    let id: Int
+    let name: String
+    let released: String
+    let background_image: String
+    let saturated_color: String
+    let platforms: [Platforms]?
+}
+
+struct Platforms: Codable, Hashable, Equatable, Comparable {
+    static func < (lhs: Platforms, rhs: Platforms) -> Bool {
+        lhs.released_at! < rhs.released_at!
+    }
+    
+    let released_at: String?
+    let platform: Platform
+}
+
+struct Platform: Codable, Hashable, Equatable, Comparable {
+    static func < (lhs: Platform, rhs: Platform) -> Bool {
+        lhs.id < rhs.id
+    }
+    
+    let id: Int
+    let name: String
+    let image: String?
+    let games_count: Int
+    let image_background: String
+    let genres: Genres?
+    
+}
+
+struct Genres: Codable, Hashable, Equatable, Comparable {
+    static func < (lhs: Genres, rhs: Genres) -> Bool {
+        lhs.name < rhs.name
+    }
+    let name: String
+    let image_background: String
+    let store: Stores
+}
+
+struct Stores: Codable, Hashable, Equatable, Comparable {
+    static func < (lhs: Stores, rhs: Stores) -> Bool {
+        lhs.domain < rhs.domain
+    }
+    
+    let name: String
+    let domain: String
+    let image_background: String
+    let tags: Tags
+}
+
+struct Tags: Codable, Hashable, Equatable, Comparable {
+    static func < (lhs: Tags, rhs: Tags) -> Bool {
+        lhs.language < rhs.language
+    }
+    
+    let name: String
+    let language: String
+    let image_background: String
 }
---
+++ GameChooserDemo/Model/Data2.swift
@@ -0,0 +1,10 @@
+//
+//  Data2.swift
+//  GameChooser
+//
+//  Created by Gavin Jensen on 10/24/22.
+//
+
+import Foundation
+
+
